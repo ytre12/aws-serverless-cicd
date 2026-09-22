e903ac8c308ba01cfedb09dc1d94698dd1700c94
ytre12 <obio12yt@gmail.com>
2026-09-22 20:46:33 +0300

    Delete cmtr_v7hgdxhd_lambda_sendNotification directory

--- cmtr_v7hgdxhd_lambda_sendNotification/handler.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-    print(f"[MOCK] Notification sent for order {event['orderId']}")
-
-    return {
-        "message": "Notification sent",
-        "orderId": event["orderId"],
-        "verification": event.get("verification")
-    }
